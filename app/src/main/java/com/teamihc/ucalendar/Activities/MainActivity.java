978df28cbad22685573ac15b942c7084070d2d91
Luisa <lmlopez.19@est.ucab.edu.ve>
2021-01-18 19:33:46 -0400

    Creación de navigation bar y top bar

---
+++ app/src/main/java/com/teamihc/ucalendar/Activities/MainActivity.java
@@ -0,0 +1,58 @@
+package com.teamihc.ucalendar.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.Dialog;
+import android.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.teamihc.ucalendar.Fragments.AgendaFragment;
+import com.teamihc.ucalendar.Fragments.CalendarioFragment;
+import com.teamihc.ucalendar.Fragments.InicioFragment;
+import com.teamihc.ucalendar.R;
+
+public class MainActivity extends AppCompatActivity
+{
+    private Toolbar toolbar;
+    Dialog dialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar=findViewById(R.id.topbar);
+        setSupportActionBar(toolbar);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_bar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        dialog = new Dialog(this);
+
+    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener= new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment =null;
+            switch (item.getItemId())
+            {
+                case R.id.nav_home: {
+                   fragment= new InicioFragment();
+                    break;
+                }
+                case R.id.nav_agenda: {
+                    fragment= new AgendaFragment();
+                    break;
+                }
+                case R.id.nav_calendario: {
+                    fragment= new CalendarioFragment();
+                    break;
+                }
+            }
+            getFragmentManager().beginTransaction().replace(R.id.layout_principal,fragment).commit();
+            return true;
+        }
+    };
+}
