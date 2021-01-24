2322416f87db3b92e91dbce5435bb0931c155c92
Luis Barreto <luis.barreto.marin@gmail.com>
2021-01-24 14:54:51 -0400

    Ocultando claves

--- servicios/conexion.php
+++ servicios/conexion.php
@@ -3,17 +3,17 @@
 $host = 'remotemysql.com';
 $db = 'HOSiKYC1GP';
 $user = 'HOSiKYC1GP';
-$pass = 'FuqcaYZhzc';
+$pass = '-';
 
 $conexion = new mysqli($host, $user, $pass, $db);
 
 if($conexion->connect_errno)
 {
-    echo "Error al conectarse";
+    echo "Error al conectarse. ";
 }
 else
 {
-    echo "Conectado a la bbdd";
+    echo "Conectado a la bbdd. ";
 }
 
 ?>
