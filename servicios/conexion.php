81076b73c6e0be7b47d3e757e664d1ee8863b42f
Luis Barreto <luis.barreto.marin@gmail.com>
2021-01-23 22:35:49 -0400

    Corrección de clave
    Tenía rato con la clave mala xddd soy bobo

--- servicios/conexion.php
+++ servicios/conexion.php
@@ -3,11 +3,17 @@
 $host = 'remotemysql.com';
 $db = 'HOSiKYC1GP';
 $user = 'HOSiKYC1GP';
-$pass = '2tkg08jx'
+$pass = 'FuqcaYZhzc';
 
 $conexion = new mysqli($host, $user, $pass, $db);
 
-if($conexion ->connect_errno)
+if($conexion->connect_errno)
 {
     echo "Error al conectarse";
 }
+else
+{
+    echo "Conectado a la bbdd";
+}
+
+?>
