78897de692a510b69b5d891ca2d44433edea0998
Karen Morán <kamoran.19@est.ucab.edu.ve>
2021-01-27 01:32:24 -0400

    Avance recycler agenda

---
+++ app/src/main/java/com/teamihc/ucalendar/adapters/AgendaRVAdapter.java
@@ -0,0 +1,38 @@
+package com.teamihc.ucalendar.adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AgendaRVAdapter{/* extends RecyclerView.Adapter<AgendaRVAdapter.AgendaAdapter> implements View.OnClickListener{
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @NonNull
+    @Override
+    public AgendaAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AgendaAdapter holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class AgendaAdapter extends RecyclerView.ViewHolder {
+
+        public AgendaAdapter(@NonNull View itemView) {
+            super(itemView);
+        }
+    }*/
+}
--- app/src/main/java/com/teamihc/ucalendar/fragments/AgendaFragment.java
+++ app/src/main/java/com/teamihc/ucalendar/fragments/AgendaFragment.java
@@ -6,17 +6,47 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.teamihc.ucalendar.R;
+import com.teamihc.ucalendar.adapters.AgendaRVAdapter;
+import com.teamihc.ucalendar.adapters.FeedRVAdapter;
+
+import java.util.ArrayList;
 
 
 public class AgendaFragment extends Fragment
 {
-    
+    private RecyclerView recyclerView;
+    private AgendaRVAdapter adapter;
+    //private ArrayList<Evento> listaEventosGuardados;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_agenda, container, false);
+        View view = inflater.inflate(R.layout.fragment_agenda, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerAgenda);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false));
+        recyclerView.getLayoutManager().setMeasurementCacheEnabled(false);
+
+        // Set adapter
+        return view;
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        /*listaEventosGuardados.clear();
+        adapter.notifyDataSetChanged();
+        Actualizar lista
+        adapter = new AgendaRVAdapter(listaArticulos, R.layout.view_info_producto);
+        recyclerView.setAdapter(adapter);
+        Articulo.cargarInventarioEnLista(listaArticulos);
+
+        ColocarBienvenida();*/
     }
 }
